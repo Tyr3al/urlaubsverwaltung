52d0696208b6ba30cbc2e65aabf5d5fb2a75d521
Andreas Weigel <weigel@synyx.de>
2021-03-01 21:56:53 +0100

    add HolidayReplcementDto and first request roundtrip

--- src/main/java/org/synyx/urlaubsverwaltung/application/web/ApplicationForLeaveForm.java
+++ src/main/java/org/synyx/urlaubsverwaltung/application/web/ApplicationForLeaveForm.java
@@ -3,6 +3,7 @@ package org.synyx.urlaubsverwaltung.application.web;
 import org.synyx.urlaubsverwaltung.application.domain.Application;
 import org.synyx.urlaubsverwaltung.application.domain.VacationCategory;
 import org.synyx.urlaubsverwaltung.application.domain.VacationType;
+import org.synyx.urlaubsverwaltung.holidayreplacement.HolidayReplacementDto;
 import org.synyx.urlaubsverwaltung.period.DayLength;
 import org.synyx.urlaubsverwaltung.person.Person;
 
@@ -10,6 +11,13 @@ import java.math.BigDecimal;
 import java.sql.Time;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static java.util.Collections.emptyList;
+import static java.util.Optional.ofNullable;
+import static java.util.stream.Collectors.toList;
 
 
 /**
@@ -43,6 +51,9 @@ public class ApplicationForLeaveForm {
 
     // Stands in while the person is on holiday
     private Person holidayReplacement;
+
+    private List<Person> holidayReplacementsSelection;
+    private List<HolidayReplacementDto> holidayReplacements;
 
     private String holidayReplacementNote;
 
@@ -235,6 +246,30 @@ public class ApplicationForLeaveForm {
 
     public void setId(Integer id) {
         this.id = id;
+    }
+
+    public List<Person> getHolidayReplacementPersons() {
+
+        return ofNullable(holidayReplacements)
+            .orElse(emptyList()).stream()
+            .map(HolidayReplacementDto::getPerson)
+            .collect(toList());
+    }
+
+    public List<HolidayReplacementDto> getHolidayReplacements() {
+        return holidayReplacements;
+    }
+
+    public void setHolidayReplacements(List<HolidayReplacementDto> holidayReplacements) {
+        this.holidayReplacements = holidayReplacements;
+    }
+
+    public List<Person> getHolidayReplacementsSelection() {
+        return holidayReplacementsSelection;
+    }
+
+    public void setHolidayReplacementsSelection(List<Person> holidayReplacementsSelection) {
+        this.holidayReplacementsSelection = holidayReplacementsSelection;
     }
 
     public static class Builder {
